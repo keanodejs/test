c620ce9611436a69fc87f176c3726801b243362f
Claus Bové <clbo@kea.dk>
2016-05-12 23:13:09 +0200

    test2

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-var express = require('express');
-var app = express();
-
-
-app.get('/', function (req, res) {
-	res.send({'msg' : 'Hello world'});
-	// test 33
-});
-
-
-app.listen(3000);
+console.log("hello world");
